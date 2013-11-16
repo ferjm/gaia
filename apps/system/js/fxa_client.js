8a69a5e89b80fc5e7eafdca011a832521bbd3501
Fernando Jiménez <ferjmoreno@gmail.com>
2013-11-16 19:00:29 +0100

    Update FxA client and manager with latest gecko changes

--- apps/system/js/fxa_client.js
+++ apps/system/js/fxa_client.js
@@ -16,7 +16,7 @@ var FxAccountsClient = function FxAccountsClient() {
 
   var sendMessage = function sendMessage(message, successCb, errorCb) {
     if (!eventCount) {
-      window.addEventListener('mozChromeEvent', onChromeEvent);
+      window.addEventListener('mozFxAccountsChromeEvent', onChromeEvent);
     }
 
     var id = getUUID();
@@ -27,12 +27,11 @@ var FxAccountsClient = function FxAccountsClient() {
 
     var details = {
       id: id,
-      type: 'fxa-service',
       data: message
     };
 
     var event = document.createEvent('CustomEvent');
-    event.initCustomEvent('mozContentEvent', true, true, details);
+    event.initCustomEvent('mozFxAccountsContentEvent', true, true, details);
     window.dispatchEvent(event);
 
     eventCount++;
@@ -41,24 +40,23 @@ var FxAccountsClient = function FxAccountsClient() {
   var onChromeEvent = function onChromeEvent(event) {
     var message = event.detail;
 
-    if (message.type != 'fxa-service' || !message.id) {
+    if (!message.id) {
+      console.log('Got mozFxAccountsChromeEvent with no id');
       return;
     }
 
     var callback = callbacks[message.id];
-    if (message.data &&
-        callback.successCb && callback.successCb instanceof Function) {
+    if (typeof message.data !== 'undefined' && callback.successCb) {
       callback.successCb(message.data);
       delete callbacks[message.id];
-    } else if (message.error &&
-               callback.errorCb && callback.errorCb instanceof Function) {
+    } else if (message.error && callback.errorCb) {
       callback.errorCb(message.error);
       delete callbacks[message.id];
     }
 
     eventCount--;
     if (!eventCount) {
-      window.removeEventListener('mozChromeEvent', onChromeEvent);
+      window.removeEventListener('mozFxAccountsChromeEvent', onChromeEvent);
     }
   };
 
--- shared/js/fxa_iac_client.js
+++ shared/js/fxa_iac_client.js
@@ -39,11 +39,13 @@ var FxAccountsIACHelper = function FxAccountsIACHelper() {
   // to a single manifest.
   var sendMessage = function sendMessage(message, successCb, errorCb) {
     getSelf(function onApp(app) {
+      if (!errorCb || typeof errorCb !== 'function') {
+        errorCb = function() {};
+      }
+
       app.connect(CONNECTION_STRING, rules).then(function(ports) {
         if (!ports || ports.length !== 1) {
-          if (errorCb && typeof(errorCb) === 'function') {
-            errorCb();
-          }
+          errorCb();
           return;
         }
 
@@ -53,7 +55,7 @@ var FxAccountsIACHelper = function FxAccountsIACHelper() {
         port.onmessage = function onMessage(evt) {
           if (evt && evt.data) {
             var realMessage = evt.data;
-            if (realMessage.data) {
+            if (typeof realMessage.data !== 'undefined') {
               successCb(realMessage.data);
             } else {
               var errorType = realMessage.error || 'Unknown';
