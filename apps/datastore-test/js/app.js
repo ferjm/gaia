bb5c68d8194c9e7c45e0a1686cc4e6e76b4dac02
Fernando Jiménez <ferjmoreno@gmail.com>
2014-04-28 19:33:29 +0200

    Add comms providers test apps

---
+++ apps/datastore-test/js/app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+function writeStore(store, callback) {
+  console.log('Adding to store ' + store);
+  store.add({
+    contacts: {
+      name: 'Comms provider 1',
+      activities: [{
+        info: 'Action 1',
+        name: 'comms-provider-1',
+        filters: [{
+          required: false,
+          type: 'tel'
+        }, {
+          required: false,
+          type: 'email'
+        }]
+      }]
+    }
+  })
+  .then(function(id) {
+    console.log('Object with id ' + id + ' added to the datastore');
+    callback();
+  });
+}
+
+function getStore(manifestURL, callback) {
+  navigator.getDataStores('comms-provider')
+  .then(function(stores) {
+    var length = stores.length;
+    if (length < 1) {
+      console.warning('No stores found');
+      return;
+    }
+    console.log('Stores ' + length);
+    while (length > 0) {
+      var store = stores[length - 1];
+      console.log('Store owner ' + store.owner + '  ' + manifestURL);
+      if (store.owner === manifestURL) {
+        writeStore(store, callback);
+        return;
+      }
+      length--;
+    }
+
+    console.warning('WTF! we do not own a comms-provider datastore');
+  }, function(error) {
+    console.error('Error getting datastores ' + error.name);
+  });
+}
+
+window.addEventListener('DOMContentLoaded', function onload() {
+  window.removeEventListener('DOMContentLoaded', onload);
+
+  // Activity handler.
+  navigator.mozSetMessageHandler('activity', function(activityRequest) {
+    document.getElementById('activity-data').textContent =
+      JSON.stringify(activityRequest.source);
+  });
+
+  if (localStorage.ds) {
+    console.log('Datastore already created');
+    return;
+  }
+
+  console.log('We need to create the datastore');
+
+  var req = navigator.mozApps.getSelf();
+  req.onsuccess = function() {
+    if (!req.result || !req.result.manifestURL) {
+      console.error('Holy crap!');
+      return;
+    }
+    getStore(req.result.manifestURL, function() {
+      localStorage.ds = true;
+    });
+  };
+  req.onerror = function() {
+    console.error('Error ' + req.error.name);
+  };
+
+});
